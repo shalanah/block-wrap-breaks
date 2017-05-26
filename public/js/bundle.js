423de3a74f426ada9422cd3a921095d0fa6ca0a0
shalanah <shalanahfaith@gmail.com>
2017-05-26 10:30:32 -0500

    Move browser tests out of getWrapIndex to perform func less times

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -1,5 +1,7 @@
 'use strict';
 
+var _slicedToArray = function () { function sliceIterator(arr, i) { var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"]) _i["return"](); } finally { if (_d) throw _e; } } return _arr; } return function (arr, i) { if (Array.isArray(arr)) { return arr; } else if (Symbol.iterator in Object(arr)) { return sliceIterator(arr, i); } else { throw new TypeError("Invalid attempt to destructure non-iterable instance"); } }; }();
+
 // TODO: Turn into one big constructor
 
 /**
@@ -18,9 +20,22 @@ var isSafari = function isSafari() {
   return navigator.userAgent.indexOf('Safari') !== -1 && navigator.userAgent.indexOf('Chrome') === -1;
 };
 
+/**
+ * Determines whether or not browser is Safari 10 and greater
+ * @returns {Bool}
+ */
 var isSafari10OrGreater = function isSafari10OrGreater() {
-  var ua = navigator.userAgent;
-  return isSafari() && ua.substr(ua.lastIndexOf('Version/') + 8, 2) === '10';
+  if (isSafari()) {
+    var ua = navigator.userAgent;
+    var versionEx = /Version\/([\d|\.]+)/g;
+
+    var _versionEx$exec = versionEx.exec(ua),
+        _versionEx$exec2 = _slicedToArray(_versionEx$exec, 2),
+        version = _versionEx$exec2[1];
+
+    console.log(Number(version) >= 10);
+    return Number(version) >= 10;
+  } else return false;
 };
 
 /**
@@ -75,16 +90,16 @@ var getTextElems = function getTextElems(block) {
  * - Does not included first line
  * @returns {Number}
  */
-var getWrapIndex = function getWrapIndex(rangeIndex, elemIndex, charCount, strLength) {
+var getWrapIndex = function getWrapIndex(rangeIndex, elemIndex, charCount, strLength, browserCase) {
   var wrapPosition = void 0;
 
-  // TODO: explain this further
-  if (isFirefox()) {
+  // TODO: Explain or document this further
+  if (browserCase === 'FF') {
     // FF:
     if (rangeIndex === 0) return; // Skip collapsed selections
     if (rangeIndex === 1 && elemIndex === 0) return; // Skip first char in block
     wrapPosition = 'wrapChar';
-  } else if (isSafari10OrGreater()) {
+  } else if (browserCase === 'Safari10OrGreater') {
     // Safari > 10
     if (rangeIndex === 0 && elemIndex === 0) return; // Skip first char in block
     if (strLength - 1 < charCount + rangeIndex) return; // Make sure wrapIndex exists for beforeWrap
@@ -117,6 +132,11 @@ var getWrapIndex = function getWrapIndex(rangeIndex, elemIndex, charCount, strLe
  * @returns {Array}
  */
 var getWrapReturns = function getWrapReturns(block) {
+  // Browser case
+  var browserCase = void 0;
+  if (isFirefox()) browserCase = 'FF';
+  if (isSafari10OrGreater()) browserCase = 'Safari10OrGreater';
+
   var textElems = getTextElems(block);
   var strLength = block.textContent.length;
   var wrapReturns = [];
@@ -155,7 +175,7 @@ var getWrapReturns = function getWrapReturns(block) {
       // Detecting start of line 
       if (isStartOfLine(top, bottom, currentTop, currentBottom)) {
         // Add wrap index (only skips adding first char in paragraph)
-        var wrapIndex = getWrapIndex(i, elemIndex, charCount, strLength);
+        var wrapIndex = getWrapIndex(i, elemIndex, charCount, strLength, browserCase);
         if (wrapIndex !== undefined) wrapReturns.push(wrapIndex);
 
         // Set next top/bottom
